3655536387121793d0d62522573e4e58ba2c4e59
Miles P. Olson <mpolson64@gmail.com>
2016-10-18 21:17:37 -0700

    Created base site

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+var fs = require('fs');
 var express = require('express');
 app = express();
 
+var adjectives = fs.readFileSync('adjectives.txt').toString().split('\n');
+var animals = fs.readFileSync('animals.txt').toString().split('\n');
+
+function getName() {
+    return adjectives[Math.floor(Math.random() * adjectives.length)] + " " + animals[Math.floor(Math.random() * animals.length)];
+}
+
 app.get('/', function(req, res) {
-    res.send('Test');
+    res.send(getName());
 });
 
 var server = app.listen(8080, function() {
